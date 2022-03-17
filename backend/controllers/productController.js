a52e84657d22a0043662fd80357c52b3f438ad09
Vishal kumar singh <uvcanvishal@gmail.com>
2022-03-17 16:00:38 +0530

    added pagination to backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -15,12 +15,16 @@ exports.createProduct = catchAsyncErrors(async (req,res,next)=>{
 // Get All Product
 exports.getAllProducts = catchAsyncErrors(async (req,res)=>{
 
-    const apiFeature = new ApiFeatures(Product.find(),req.query).search().filter();
+    const resultPerPage = 5;
+    const productCount = await Product.countDocuments();
+
+    const apiFeature = new ApiFeatures(Product.find(),req.query).search().filter().pagination(resultPerPage);
     const products = await apiFeature.query;
 
     res.status(200).json({
         success:true,
-        products
+        products,
+        productCount
     })
 });
 
@@ -35,7 +39,7 @@ exports.getProductDetails = catchAsyncErrors(async (req,res,next)=>{
    
     res.status(200).json({
         success:true,
-        product
+        product,
     })
 
 });
